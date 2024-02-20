5492776e5bc4ffd0e87029a7d761acac54ff8b1f
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-02-20 10:08:00 +0530

    first commit

---
+++ app/home/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Container from "@/components/Container";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <Container>
+        <Navbar />
+        <h1>Home</h1>
+      </Container>
+    </>
+  );
+}
+
+export default Home;
